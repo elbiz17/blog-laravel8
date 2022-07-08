4e83ad5752e9712c9963d8eff7ac3573f7c877a1
elbiz17 <l699654@gmail.com>
2022-07-08 23:09:13 +0700

    make the sidebar who can use by admin

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -328,6 +328,21 @@ trix-toolbar [data-trix-button-group="file-tools"]{
             </a>
           </li>
         </ul>
+        
+        {{-- gate admin --}}
+        @can('admin')
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1">
+            <span>Administrator</span>
+        </h6>
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }}" aria-current="page" href="/dashboard/categories">
+              <span data-feather="home"></span> <i class="bi bi-file-text"></i>
+              Post Categories
+            </a>
+          </li>
+        </ul>
+        @endcan
       </div>
     </nav>
 
