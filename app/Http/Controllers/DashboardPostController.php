8db5ef2a474e16a2a580fc576e73a47ede1fd1a4
elbiz17 <l699654@gmail.com>
2022-07-08 10:03:40 +0700

    add condition upload image

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -52,15 +52,22 @@ class DashboardPostController extends Controller
         //
         $message = [
             'required' => 'wajib diisi ya bang',
-            'max' => ':attribute maksimal diisi :max karakter'
+            'max' => ':attribute maksimal :max kb'
         ];
+
+        // return $request->file('image')->store('post-images');
 
         $validatedData = $request->validate([
             'title' => 'required|max:255',
             'slug' => 'required|unique:posts',
             'category_id' => 'required',
+            'image' => 'image|file|max:1024',
             'body' => 'required'
         ], $message);
+
+        if($request->file('image')){
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
         
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
