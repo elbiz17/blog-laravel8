7d57acae94040f75c14dccc104e60fd91d409c3a
elbiz17 <l699654@gmail.com>
2022-07-08 18:16:07 +0700

    menambahkan enctype di form

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -19,7 +19,7 @@
         <div class="row mb-5">
             <div class="col-lg-8 mb-5">
 
-                <form method="post" action="/dashboard/posts/{{ $post->slug }}"> 
+                <form method="post" action="/dashboard/posts/{{ $post->slug }}" enctype="multipart/form-data"> 
                     @csrf
                     @method('PUT')
                     <div class="mb-3">
@@ -51,7 +51,27 @@
                             @endif
                           @endforeach               
                       </select>
-                    </div>            
+                    </div>      
+                    <div class="mb-3">
+                      <label for="image" class="form-label">Post Image</label>
+                      <input type="hidden" name="oldImage" value="{{ $post->image }}">
+                      @if ($post->image)
+                      {{-- <div class="img-fluid" style="max-height: 150px; max-width:320px"> --}}
+                        <img src="{{ asset('storage/' . $post->image) }}" class="img-preview img-fluid pb-3 d-block" style="max-height: 150px; max-width:320px" alt="{{ $post->image }}">
+
+                      {{-- </div> --}}
+
+                      @else
+                        <img class="img-preview img-fluid pb-3 " style="max-height: 150px; max-width:320px">
+                          
+                      @endif
+                      <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image"  required onchange="previewImage()">
+                      @error('image')
+                      <div class="inavlid-feedback">
+                        {{ $message }}
+                      </div>
+                      @enderror
+                    </div>         
                     <div class="mb-3">
                       <label for="body" class="form-label">Body</label>
     
@@ -86,5 +106,19 @@
     document.addEventListener('trix-file-accept', function(e){
         e.preventDefault();
     });
+
+    function previewImage(){
+      const image = document.querySelector('#image');
+      const imgPreview = document.querySelector('.img-preview');
+
+      imgPreview.style.display = 'block';
+ 
+      const oFReader = new FileReader();
+      oFReader.readAsDataURL(image.files[0]);
+
+      oFReader.onload = function(oFREvent){
+        imgPreview.src = oFREvent.target.result;
+      }
+    }
 </script>
 @endsection
