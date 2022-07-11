a23da14c28446974c7f9d4ed86fd0cce0534e9fb
elbiz17 <l699654@gmail.com>
2022-07-11 20:55:04 +0700

    edit view news

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -22,9 +22,17 @@
                          </a>
                       </div>
                       <div class="image">
-                         <a class="text-decoration-none" href="/show/{{ $posts[0]->slug }}">
-                            <img class="img-fluid" src="https://source.unsplash.com/random/400x200/?{{ $posts[0]->category->name }}" alt="{{ $posts[0]->category->name }}" style="width:100%">
-                         </a>
+                        <a class="text-decoration-none" href="/show/{{ $posts[0]->slug }}">
+                           @if ($posts[0]->image)
+                           <div style="max-height:350px;overflow:hidden">
+                              <img src="{{ asset('storage/' . $posts[0]->image) }}" alt="" style="width: 100%">
+                           </div>
+                           @else
+                              <img src="https://source.unsplash.com/random/450x200/?{{ $posts[0]->category->name }}" alt="{{ $posts[0]->category->name }}" style="width:100%">
+   
+                               
+                           @endif
+                        </a>
                       </div>
                         
                       <div class="card-body">
@@ -86,9 +94,9 @@
                </div>
             </div>
             <div class="px-1">
-               <div class="row">
+               <div class="row row-cols-2">
                   @foreach ($posts->skip(1) as $post)
-                  <div class="col-md-4 py-2 px-1">
+                  <div class="col-md-4 pt-2 px-1">
                      <div class="berita flex" style="height: auto">
                         <div class="contents">
                            <div class="position-absolute px-3 py-2 text-white" style="background-color: rgba(0,0,0,0.3);border-top-left-radius:8px;z-index:999">
@@ -98,7 +106,15 @@
                            </div>
                            <div class="image">
                               <a class="text-decoration-none" href="/show/{{ $post->slug }}">
-                                 <img src="https://source.unsplash.com/random/200x100/?{{ $post->category->name }}" alt="">
+                                 @if ($post->image)
+                                 <div style="max-width:320px; max-height:150px;overflow:hidden">
+                                    <img src="{{ asset('storage/' . $post->image) }}" alt="" style="width: 100%">
+                                 </div>
+                                 @else
+                                    <img src="https://source.unsplash.com/random/320x150/?{{ $post->category->name }}" alt="">
+         
+                                     
+                                 @endif
                               </a>
                            </div>
                            <div class="card-body">
@@ -129,10 +145,10 @@
                   </div>
                     
                 @endforeach
-                <div class="loadMore">
-                  <a href="#" id="loadMore">Load More</a>
                </div>
-               </div>
+               <div class="loadMore">
+                 <a href="#" id="loadMore">Load More</a>
+              </div>
             </div>
          </div>
      
